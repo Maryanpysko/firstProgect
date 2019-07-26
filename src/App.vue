7c878c938c1dc6c30f7f96e785feed5707ca34c4
Maryan Pysko <Maryanpysko@gmail.com>
2019-07-26 21:45:30 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="collapse navbar-collapse" >
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/cars">Cars</router-link>
+            </li >
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Пошук">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Пошук</button>
+          </form>
+        </div>
+      </nav> 
+      <router-view></router-view>
+  </div>
+    
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="futer">
+			<h2 class="collection-title">Image Collection<small>Visual Quality</small></h2>
+			<div class="collections">
+				<div class="collection-item-outer">
+					<div class="collection-item">
+						<img src="image/Lol.jpg"alt="List picture" width="300" height>
+						<div class="collections-text">
+							<h3>List Element Itemc</h3>
+							<p>Images</p>
+							<button>Read More...</button>
+						</div>
+					</div>
+				</div>
+				<div class="collection-item-outer">
+					<div class="collection-item">
+						<img src="src\image\Lol.jpg" alt="List picture" width="300" height>
+						<div class="collections-text">
+							<h3>List Element Itemc</h3>
+							<p>Images</p>
+							<button>Read More...</button>
+						</div>
+					</div>
+				</div>
+				<div class="collection-item-outer">
+					<div class="collection-item">
+						<img src="src\image\Lol.jpg" alt="List picture" width="300" height>
+						<div class="collections-text">
+							<h3>List Element Itemc</h3>
+							<p>Images</p>
+							<button>Read More...</button>
+						</div>
+					</div>
+				</div>
+			</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+.collections {
+	overflow: auto;
+}
+
+.collection-item-outer {
+	display: inline-block;
+	float: left;
+	padding-left: 5px;
+	padding-right: 5px;
+	width: 33.33%;
+	box-sizing: border-box;
+}
+.collections > div:first-child {
+	padding-left: 0;
+	padding-right: 10px;
+}
+.collections > div:last-child {
+	padding-right: 0;
+	padding-left: 10px;
+}
+.collection-item {
+	border: 1px solid #959595;
+}
+.collection-item img,
+.collection-item video {
+	width: 100%;
+}
+.collection-text {
+	padding: 0 20px 20px 20px;
+}
+.collection-text h3 {
+	color: yellowgreen;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+}
+.collection-text p {
+	height: 80px;
+	overflow: hidden;
+}
+.collection-text button {
+	padding: 10px;
+	border: 1px solid transparent;
+	border-radius: 5px;
+	outline: none;
+}
+
+</style>
+
+
+
